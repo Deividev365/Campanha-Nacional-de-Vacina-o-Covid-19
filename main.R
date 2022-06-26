c635c551213ad91db0f293af7a9dfd18e20043d9
Deivid <deividalmeida365@gmail.com>
2022-06-26 15:06:12 +0000

    FEAT: database added

--- main.R
+++ main.R
@@ -1,22 +1,6 @@
-#Suponha que a pressão arterial de uma pessoa mede tipicamente 160 ± 20 mm. Se
-#alguém fizer n = 5 leituras de pressão arterial, qual é a probabilidade de a média ser <=
-#150?
+
+INFLUD18 <- read_csv("INFLUD18.csv", header = TRUE, sep = ",")
 
 
-#Probabilidade
-pnorm(q = 150, mean = 160, sd = 20 / sqrt(5), lower.tail = TRUE)
 
-
-#Gráfico
-data.frame(x = 100:200, 
-           prob = pnorm(q = 100:200, 
-                        mean = 160, 
-                        sd = 20 / sqrt(5), 
-                        lower.tail = TRUE)) %>%
-  mutate(cdf = ifelse(x > 0 & x <= 150, prob, 0)) %>%
-  ggplot() +
-  geom_line(aes(x = x, y = prob)) +
-  geom_area(aes(x = x, y = cdf), alpha = 0.3) +
-  labs(title = bquote('X~N('~mu==.(160)~','~sigma^{2}==.(20 / sqrt(5))^{2}~')'),
-       x = "x",
-       y = "Probabilidade acumulada")
+INFLUD18
